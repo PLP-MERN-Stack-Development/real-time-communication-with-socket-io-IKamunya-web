dce4adf78b89bc98b024f60cf9ed23aabcdb8a8f
Stephen Nafula <stephenbranny@gmail.com>
2025-11-10 16:06:42 +0300

    UI: remove people/MembersList sidebar, keep channels only

--- client/src/components/DiscordLayout.jsx
+++ client/src/components/DiscordLayout.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ChannelList from './ChannelList';
-import MembersList from './MembersList';
 
 const DiscordLayout = ({ rooms, currentRoom, switchRoom, users, username, children, onPrivateMessage }) => (
   <div className="flex min-h-screen w-full">
@@ -8,8 +7,7 @@ const DiscordLayout = ({ rooms, currentRoom, switchRoom, users, username, childr
       <div className="p-3 border-b border-discord-gray-800">
         <h1 className="text-white text-lg font-bold">Workspace</h1>
       </div>
-      <ChannelList rooms={rooms} currentRoom={currentRoom} switchRoom={switchRoom} />
-      <MembersList users={users} username={username} onPrivateMessage={onPrivateMessage} />
+  <ChannelList rooms={rooms} currentRoom={currentRoom} switchRoom={switchRoom} />
     </aside>
     <main className="flex-1 bg-discord-gray-950 text-white flex flex-col">
       {children}
